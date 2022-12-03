740d0831aea88fa8adbeef06b4e8770835c6a09e
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-12-03 13:34:01 +0600

    Worked with the sample question where checking the loop, function and condition.

---
+++ Python_Basic_Certification_Test/sample_fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzBuzz(n:int):
+    if (n%3==0) and (n%5==0):
+        print('FizzBuzz')
+    elif (n%3==0):
+        print('Fizz')
+    elif (n%5==0):
+        print('Buzz')
+    else:
+        print(n)
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        fizzBuzz(i)
